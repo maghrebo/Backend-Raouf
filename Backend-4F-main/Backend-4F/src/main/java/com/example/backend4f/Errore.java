689f458b5cdacf9e2a2740430cb40afd3e012ebb
Amine <raouf.amine@iisgalvanimi.edu.it>
2024-05-02 14:50:45 +0200

    prima modifica punto 4

--- Backend-4F-main/Backend-4F/src/main/java/com/example/backend4f/Errore.java
+++ Backend-4F-main/Backend-4F/src/main/java/com/example/backend4f/Errore.java
@@ -1,12 +1,11 @@
 package com.example.backend4f;
 
 import java.io.*;
-
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+@WebServlet(name = "errore", value = "/errore")
+public class Errore extends HttpServlet {
     private String message;
 
     public void init() {
@@ -14,13 +13,15 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
+
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+
+
     }
 
     public void destroy() {
